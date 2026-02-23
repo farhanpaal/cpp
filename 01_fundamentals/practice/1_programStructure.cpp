f69513ba1230900fd5aefedad05c559e64ad008c
FARHAN PALA <farhan.12345.pala@gmail.com>
2026-02-23 16:51:42 +0530

    added

---
+++ 01_fundamentals/practice/1_programStructure.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#include <string>;
+
+int main()
+{
+    // int a = 5;
+    // char color;
+
+    // cout << "enter number";
+    // cin >> a;
+    // cout << "the number is: "
+    //      << a;
+
+    // if (a > 18)
+    // {
+    //     cout << "\nYou are an adult";
+    // }
+    // else
+    // {
+    //     cout << "\nYou are minor";
+    // }
+
+    // cout << "\n Which color do you like: ";
+    // cin >> color;
+    // switch (color)
+    // {
+    // case 'r':
+    //     cout << "red";
+    //     break;
+    // case 'g':
+    //     cout <<"green";
+    //     break;
+    // case 'b':
+    //     cout<< "blue";
+    //     break;
+    // }
+
+    cout<<"===================================\n";
+    cout<<"======YOUR SCORE CARD ENTRY========\n";
+    string subjects[]={"math", "science","urdu","english", "sst"};
+    subjects[0]="55";
+    cout<<subjects[0];
+
+}
---
+++ 01_fundamentals/practice/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter number: ");
+    scanf("%d",&a);
+    printf("The number is %d", a);
+}
