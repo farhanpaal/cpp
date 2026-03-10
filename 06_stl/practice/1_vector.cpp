3bc4cf1ba68c97510b28b4912aaceb8f6e3c3211
FARHAN PALA <farhan.12345.pala@gmail.com>
2026-03-10 13:09:21 +0530

    added

---
+++ 06_stl/practice/1_vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+    vector<string> myCars = {"hayabusa", "lambo"};  
+
+    for (string car : myCars)
+    {
+        cout << car << " ";
+    }
+}
