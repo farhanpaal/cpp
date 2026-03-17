551fb6b7c720efef64dd821b6569db3e383c943b
FARHAN PALA <farhan.12345.pala@gmail.com>
2026-03-17 17:57:24 +0530

    added list stl

---
+++ 06_stl-DSA/practice/2_list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+    // we can't access using index, but we can access first and last using .front() .back() func
+    list<string> cars = {"volvo", "scorpio", "lamborghini", "Buggati"};
+    // print all:
+    for (string car : cars)
+    {
+        cout << car << " - ";
+    }
+
+    cars.front() = "bolero"; // change(replace) first element.
+    cout << "print first element:" << cars.front() << endl;
+
+    // remove last element
+    cars.pop_back();
+    for (string car : cars)
+    {
+        cout << car << " - ";
+    }
+    cout << endl;
+
+    // Add last element
+    cars.push_back("revx");
+    for (string car : cars)
+    {
+        cout << car << " - ";
+    }
+    cout << endl;
+    cout << "List Size: " << cars.size();
+
+    return 0;
+}
